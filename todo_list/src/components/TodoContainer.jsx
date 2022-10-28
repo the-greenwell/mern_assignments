edc8fe5ca61026f49d197fdadd08000150eef744
the-greenwell <ant.greenwell2@gmail.com>
2022-10-27 17:58:46 -0700

    Refactor

--- todo_list/src/components/TodoContainer.jsx
+++ todo_list/src/components/TodoContainer.jsx
@@ -1,11 +1,73 @@
 import React from 'react'
 import Todo from './Todo'
+import Input from './Input';
+import { useReducer, useState, useEffect } from 'react';
+
+const saved = localStorage.getItem("todos");
+const initialState = {
+    todos: JSON.parse(saved) || []
+}
+
+const reducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case 'ADD_TODO':
+        return { ...state, todos: [...state['todos'], payload] }
+        case 'DELETE_TODO':
+        return { ...state, todos: payload }
+        case 'COMPLETE_TODO':
+        return {...state, todos: payload }
+        default:
+        return state
+    }
+}
 
 const TodoContainer = (props) => {
+    const [inputValue, setState] = useState('')
+    const [initState,dispatch] = useReducer(reducer, initialState)
+    
+    
+        useEffect(() => {
+            localStorage.setItem("todos", JSON.stringify(initState.todos))
+        }, [initState])
+    
+        const submitHandler = (e) => {
+            e.preventDefault();
+            dispatch({
+                type: 'ADD_TODO',
+                payload: {todo: inputValue, completed: false}
+            });
+        }
+    
+        const changeHandler = (e) => {
+            const { value } = e.target;
+            setState(value)
+        }
+    
+        const deleteHandler = (id) => {
+            const todos = initState.todos;
+            const payload = todos.filter(todo => todo !== todos[id]);
+            dispatch({
+                type: 'DELETE_TODO',
+                payload: payload
+            })
+        }   
+
+    const completedHandler = (id) => {
+        const todos = initState.todos;
+        todos[id] = {...todos[id], completed: !todos[id].completed}
+        dispatch({
+            type: 'COMPLETE_TODO',
+            payload: todos
+        })
+    }
+
     return (
-        <ul className='list-group mt-2'>
-            {props.todos.map((todo, i) => <Todo todo={todo.todo} completed={todo.completed} key={i} completedHandler={() => props.completedHandler(i)} deleteHandler={() => props.deleteHandler(i)}/>)}
-        </ul>
+        <>      
+            <Input submitHandler={submitHandler} changeHandler={changeHandler} value={inputValue} />
+            <ul className='list-group mt-2'>
+                {initState.todos.map((todo, i) => <Todo todo={todo.todo} completed={todo.completed} key={i} completedHandler={() => completedHandler(i)} deleteHandler={() => deleteHandler(i)}/>)}
+            </ul>
+        </>
     )
 }
 
