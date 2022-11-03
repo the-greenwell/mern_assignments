49b7178691c1dfd2de4fbabaa0e99122f12c9c71
the-greenwell <ant.greenwell2@gmail.com>
2022-11-02 17:27:54 -0700

    Faker Api, coding dojo

---
+++ faker_api/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import { faker } from '@faker-js/faker';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extende:true}));
+
+class User{
+    constructor(){
+        this._id = faker.database.mongodbObjectId()
+        this.firstName = faker.name.firstName()
+        this.lastName = faker.name.lastName()
+        this.phoneNumber = faker.phone.number('(510) ###-####')
+        this.password = faker.internet.password()
+        this.email = faker.internet.email()
+    }
+};
+class Company {
+    constructor(){
+        this._id = faker.database.mongodbObjectId()
+        this.name = faker.company.name()
+        this.address = {
+            street: faker.address.streetAddress(),
+            city: faker.address.cityName(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country()
+        }
+    }
+};
+
+app.get('/api', (req,res) => {
+    res.json({ message: 'API is running'})
+});
+app.get('/api/user/new', (req,res) => {
+    res.json({ user: new User() })
+});
+app.get('/api/company/new', (req,res) => {
+    res.json({ company: new Company() })
+});
+app.get('/api/user/company', (req,res) => {
+    res.json({ user: new User(), company: new Company() })
+});
+
+const port = 8000;
+app.listen(port, () => console.log(`Listening on port: ${port}`));
